d9dda98dbcc4e1694fd34b840d13d9f12558e271
Martí <martifarranc@gmail.com>
2025-03-14 10:33:25 +0100

    Primer commit base

---
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigation.ui.theme.NavigationTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    NavGraph(navController = navController)
+}
+

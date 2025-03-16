95e164a1593f2f3a9dfea644fa0b79ffeff36e28
Martí <martifarranc@gmail.com>
2025-03-16 13:18:07 +0100

    Modificació LoginScreen.kt (Caixa de text Username i contra + boto de register)

--- app/src/main/java/com/example/navigation/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/navigation/ui/screens/LoginScreen.kt
@@ -2,6 +2,7 @@ package com.example.navigation.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,14 +10,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+
 @Composable
 fun LoginScreen(navController: NavController) {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = "Login Screen")
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuari") }
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contrasenya") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Button(onClick = {
             // Navigate to Home and remove Login from the back stack
@@ -25,6 +51,23 @@ fun LoginScreen(navController: NavController) {
             }
         }) {
             Text(text = "Login")
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp) // Afegeix un petit marge
+    ) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomEnd), // Alinea el contingut a baix a la dreta
+            verticalAlignment = Alignment.CenterVertically // Centra verticalment el text i el botó
+        ) {
+            Text(text = "No tens compte?")
+            Spacer(modifier = Modifier.width(8.dp)) // Espai entre el text i el botó
+            Button(onClick = { navController.navigate("register") }) {
+                Text(text = "Register")
+            }
         }
     }
 }

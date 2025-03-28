543bc8755fd39145e4d66a449ba25add4886e496
nigel <nboadag@gmail.com>
2025-03-28 16:45:18 +0100

    Add DataStoreManager for user preferences and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.core)
+
+
 }
---
+++ app/src/main/java/com/example/waygo/domain/repository/ItineraryRepository.kt
@@ -0,0 +1,24 @@
+package com.example.waygo.domain.model.repository
+
+import com.example.waygo.domain.model.ItineraryItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class ItineraryRepository {
+    private val _itineraryItems = MutableStateFlow<List<ItineraryItem>>(emptyList())
+    val itineraryItems: StateFlow<List<ItineraryItem>> get() = _itineraryItems
+
+    fun addActivity(activity: ItineraryItem) {
+        _itineraryItems.value = _itineraryItems.value + activity
+    }
+
+    fun updateActivity(updatedActivity: ItineraryItem) {
+        _itineraryItems.value = _itineraryItems.value.map {
+            if (it.id == updatedActivity.id) updatedActivity else it
+        }
+    }
+
+    fun deleteActivity(activityId: String) {
+        _itineraryItems.value = _itineraryItems.value.filter { it.id != activityId }
+    }
+}

374ad141aef431a8b6f1ebc337afe970b3e82400
nigel <nboadag@gmail.com>
2025-03-29 23:45:33 +0100

    Add dependencies for AndroidX libraries and update repository settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,13 @@ android {
 
 dependencies {
 
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.navigation:navigation-compose:2.7.2")
+    implementation ("androidx.compose.material3:material3:1.1.2")
+
+
     // Jetpack Compose
     implementation ("androidx.compose.ui:ui:1.6.0")
     implementation ("androidx.compose.material3:1.2.0")
